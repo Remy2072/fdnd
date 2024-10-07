5373c5d09139bf2ee517dd6083c5a2a94740bd02
Jaymar Westerlow <jaywesterlow71@gmail.com>
2024-10-07 10:42:58 +0200

    v5.2 | components | Header to Hero 2🐛

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import Navigation from '../lib/Navigation.svelte';
-    import Header from '../lib/Header.svelte';
+    import Hero from '../lib/Hero.svelte';
     import About from '../lib/About.svelte';
     import Calendar from '../lib/Calendar.svelte';
     import Footer from '../lib/Footer.svelte';
